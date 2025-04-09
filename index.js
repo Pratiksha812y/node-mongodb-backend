26977a69c25365b19c097cd0818abdb6c2ce58d7
Pratiksha812y <yelpalepratiksha1279>
2025-04-09 15:54:36 +0530

    main

---
+++ index.js
@@ -0,0 +1,66 @@
+const express=require('express')
+const cors=require('cors')
+const bodyParser=require('body-parser')
+const mongoose=require('mongoose')
+const User = require('./models/User')
+
+const server=express()
+server.use(cors())
+server.use(bodyParser.json())
+
+mongoose.connect('mongodb+srv://pratiksha:Pratiksha%40123@leadsoft.bfw87k4.mongodb.net/').
+then(()=>console.log('Database Connected')).catch((err)=>console.log(err))
+
+
+
+server.post('/register',async(req,res)=>{
+    try{
+          const{fullName,userName,age,password}=req.body
+          const userExist=await User.findOne({userName})
+          if(userExist){
+              return res.json({
+                status:false,
+                message:"User already exist"
+              })
+          }
+          const userObj=new User({fullName,userName,age,password})
+          await userObj.save()
+          res.json({
+            status:true,
+            message:"User registered successfully"
+        })
+    }
+    catch(err){
+          res.json({
+              status:false,
+              message: err
+          })
+    }
+})
+server.post('./login',async(req,res)=>{
+    
+         const {userName,password}=req.body
+         const userExist=await User.findOne({userName})
+         if(!userExist){
+             return res.json({
+                status:false,
+                message:"User not Found"
+             })
+            }
+             if(password!==userExist.password){
+                return res.json({
+                    status:false,
+                    message:"Wrong Password"
+                })
+                
+             }
+             res.json({
+                status:true,
+                message:"Login Successful"
+             })
+            })
+
+
+server.listen(8055, () => {
+    console.log('Server started at port 8055');
+});
